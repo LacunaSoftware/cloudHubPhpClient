bae1177b54ca06ae66e19105110d2034e0989d51
Danilo Bispo <danilobispogalvao@gmail.com>
2023-01-30 10:43:23 -0300

    Added new empty array for sessionModel services to prevent errors when using the lib

--- app/CloudHub/SessionModel.php
+++ app/CloudHub/SessionModel.php
@@ -1,14 +1,21 @@
 <?php
+
 namespace Lacuna\CloudHub;
+
+use Exception;
 use Lacuna\CloudHub\TrustServiceAuthParametersModel;
 
 
-class SessionModel {
-    public ?array $services;
+class SessionModel
+{
+    public array $services = array();
 
-    public function __construct($data) {
-        foreach ($data['services'] as $key=>$service) {
-            $this->services[$key] = new TrustServiceAuthParametersModel($service);
+    public function __construct($data)
+    {
+        if (!$data['services']) {
+            foreach ($data['services'] as $key => $service) {
+                $this->services[$key] = new TrustServiceAuthParametersModel($service);
+            }
         }
     }
 }
