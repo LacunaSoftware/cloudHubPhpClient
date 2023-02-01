8be190c3c79c48c6bee1c7555121f212b5678ac8
Danilo Bispo <danilobispogalvao@gmail.com>
2023-02-01 15:09:10 -0300

    Updated client lib, changed hash type from array to string

--- app/CloudHub/SignHashRequest.php
+++ app/CloudHub/SignHashRequest.php
@@ -3,12 +3,12 @@ namespace Lacuna\CloudHub;
 
 class SignHashRequest {
     public string $session;
-    public array $hash;
+    public string $hash;
     public ?string $digestAlgorithm;
     public ?string $digestAlgorithmOid;
     public ?string $certificateAlias;
 
-    public function __construct(string $session, array $hash, string $digestAlgorithm, string $digestAlgorithmOid, string $certificateAlias) {
+    public function __construct(string $session, string $hash, string $digestAlgorithm, string $digestAlgorithmOid, string $certificateAlias) {
         $this->session = $session;
         $this->hash = $hash;
         $this->digestAlgorithm = $digestAlgorithm;
