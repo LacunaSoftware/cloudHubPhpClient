a95c59c746631915fedbae9276cf61fb024d58c6
Danilo Bispo <danilobispogalvao@gmail.com>
2023-02-01 15:38:47 -0300

    Added whitespace, tag didn't work

--- app/CloudHub/SignHashRequest.php
+++ app/CloudHub/SignHashRequest.php
@@ -8,12 +8,11 @@ class SignHashRequest {
     public ?string $digestAlgorithmOid = null;
     public ?string $certificateAlias = null;
 
-    public function __construct(string $session, string $hash, string $digestAlgorithm = null, string $digestAlgorithmOid=null, string $certificateAlias=null) {
+    public function __construct(string $session, string $hash, string $digestAlgorithm = null, string $digestAlgorithmOid = null, string $certificateAlias = null) {
         $this->session = $session;
         $this->hash = $hash;
         $this->digestAlgorithm = $digestAlgorithm;
         $this->digestAlgorithmOid = $digestAlgorithmOid;
         $this->certificateAlias = $certificateAlias;
     }
-
 }
