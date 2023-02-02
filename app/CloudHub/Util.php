739d1e1eb95351f31cc40f7105a04ba40280b932
Danilo Bispo <danilobispogalvao@gmail.com>
2023-02-02 11:17:44 -0300

    no change, just updating version with autoload referencing updated

--- app/CloudHub/Util.php
+++ app/CloudHub/Util.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace Lacuna\CloudHub;
 class Util {
     static function base64Convert(string $string): string {
-        if(!base64_decode($string, true) != false){
+        if(!base64_decode($string, true) != false) {
             return base64_encode($string);
         } else {
             return $string;
