2f22b3f258dc3fbc5cb8ad10917a2d9912b4de85
Danilo Bispo <danilobispogalvao@gmail.com>
2023-08-09 16:16:41 -0300

    Added lifetime + removed optional parameters from specification

--- app/Cloudhub/SessionCreateRequest.php
+++ app/Cloudhub/SessionCreateRequest.php
@@ -3,14 +3,16 @@ namespace Lacuna\Cloudhub;
 
 class SessionCreateRequest {
     
-    public ?string $identifier;
-    public ?string $redirectUri;
+    public string $identifier;
+    public string $redirectUri;
     public $type;
+    public int $lifetimeInSeconds;
 
-    public function __construct(string $identifier, string $redirectUri, $type) {
+    public function __construct(string $identifier, string $redirectUri, $type, $lifetimeInSeconds = null) {
         $this->identifier = $identifier;
         $this->redirectUri = $redirectUri;
         $this->type = $type;
+        $this->lifetimeInSeconds = $lifetimeInSeconds ?? null;
     }
 }
 
